f43c9635a101c93dc094fe1c7bd6f39e3a91d5ff
Sunga Thawethe <sungabt@gmail.com>
2024-06-20 11:00:04 +0200

    Create React App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navigation from './components/Navigation';
+import Profile from './components/Profile';
+import UserMain from './components/UserMain';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <Profile />
+      <UserMain />
+    </div>
+  );
+}
+
+export default App;
