5bd3bdf1f668a3b722f0fdcde45344f1a12cfed5
Sunga Thawethe <sungabt@gmail.com>
2024-06-20 11:44:07 +0200

    Add Navigation Component

---
+++ src/components/Navigation.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import '../styles/Navigation.css'
+
+const Navigation = () => {
+  return (
+    <header>
+
+    <h1>NETFLIX ZM LTD</h1>
+    <h5>Where quality meets people</h5>
+
+
+    <nav>
+      <ul className="navbar">
+        <li>
+          ORDERS
+        </li>
+        <li>
+          PRODUCTS
+        </li>
+        <li>
+          INVENTORY
+        </li>
+        <li>
+          PROFILE
+        </li>
+      </ul>
+    </nav>
+
+    </header>
+  )
+}
+
+export default Navigation;
